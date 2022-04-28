b0896cc6dc186f81a6d3cbeaf745346d07c8a466
Cazanova144 <cazpians@gmail.com>
2022-04-28 19:13:57 +0200

    Fixed movie info page

--- src/components/MovieInfoCard.jsx
+++ src/components/MovieInfoCard.jsx
@@ -1,13 +1,16 @@
 import { Card, ListGroup, Col } from "react-bootstrap"
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react"
+import getIdFromUrl from '../services/GetId'
 
-const MovieInfoCard = () => {
+const MovieInfoCard = ({ info, characters }) => {
+
     return (
         <div>
 
             <Col>
                 <Card>
-                    <Card.Header as="h1">Movie Title</Card.Header>
+                    <Card.Header as="h1">{info.title}</Card.Header>
                                 
                     <Card.Body>
                         <h2>Attributes</h2>
@@ -21,10 +24,10 @@ const MovieInfoCard = () => {
                             </div>
 
                             <div>
-                                <p>NaN</p>
-                                <p>NaN</p>
-                                <p>NaN</p>
-                                <p>NaN</p>
+                                <p>{info.episode_id}</p>
+                                <p>{info.director}</p>
+                                <p>{info.producer}</p>
+                                <p>{info.release_date}</p>
                             </div>
                         </div>
 
@@ -34,7 +37,10 @@ const MovieInfoCard = () => {
                             <p style={{ width: '30%' }}><strong>Characters</strong></p>
 
                             <ListGroup style={{ width: '70%' }}>
-                                <ListGroup.Item as={Link} to="/characters/:id">Character NaN</ListGroup.Item>
+
+                                {characters.map(character => (
+                                    <ListGroup.Item key={character} as={Link} to={`/characters/${getIdFromUrl(character)}`}>Character {getIdFromUrl(character)}</ListGroup.Item>
+                                ))}
                             </ListGroup>
                         </div>
  
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,11 +1,38 @@
 import MovieInfoCard from "../components/MovieInfoCard" 
+import SWApi from "../services/SWApi"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
 
 const Movie = () => {
+    const [info, setInfo] = useState([])
+    const [characters, setCharacters] = useState([])
+    const { id } = useParams()
+
+    const getMovieInfo = async () => {
+        setInfo([])
+
+        const data = await SWApi.getMovie(id)
+        setInfo(data)
+        setCharacters(data.characters)
+    }
+
+    useEffect(() => {
+        getMovieInfo()
+    }, [])
+
+    if (!info) {
+        return <p>Loading...</p>
+    }
+
+    if (!characters) {
+        return <p>Loading...</p>
+    }
+
     return (
         <>
             <br />
 
-            <MovieInfoCard />
+            <MovieInfoCard info={info} characters={characters} />
         </>
     )
 }
