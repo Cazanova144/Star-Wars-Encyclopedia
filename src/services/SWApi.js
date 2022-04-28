9ccc8fb80c230c8a17a427844f9e11508e5dea51
Cazanova144 <cazpians@gmail.com>
2022-04-28 18:46:40 +0200

    Fixed SWApi.js

--- src/services/SWApi.js
+++ src/services/SWApi.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://swapi.dev/api/'
+axios.defaults.baseURL = 'https://swapi.dev/api'
 
-const get = async (endpoint) => {
-    const response = await axios.get(endpoint)
-    return response.data
+const getMovies = async () => {
+    const res = await axios.get(`/films`)
+    return res.data
 }
 
-export const getIdFromUrl = (url) => {
-	// eslint-disable-next-line no-unused-vars
-	const [_endpoint, id] = url
-		.replace('https://swapi.dev/api/', '')
-		.slice(0, -1)
-		.split('/')
+const getMovie = async (id) => {
+    const res = await axios.get(`/films/${id}`)
+    return res.data
+}
 
-	return id
+const getPeople = async (page) => {
+    const res = await axios.get(`/people/?page=${page}`)
+    return res.data
+}
+
+const getPerson = async (id) => {
+    const res = await axios.get(`/people/${id}`)
+    return res.data
+}
+
+export default {
+    getMovies,
+    getMovie,
+    getPeople,
+    getPerson,
 }
