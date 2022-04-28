5a2a61c59b7569ffc5ff9e40167904bda4c74669
Cazanova144 <cazpians@gmail.com>
2022-04-28 10:46:35 +0200

    Fixed /movies site

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,34 +1,37 @@
 import { Card, ListGroup, Button, Col } from "react-bootstrap"
 import { Link } from 'react-router-dom'
+import getIdFromUrl from '../services/SWApi'
 
-const MovieCard = () => {
+const MovieCard = ({ movies }) => {
+
     return (
-        <div style={{ width: '27rem' }} className="mb-5">
+        <div style={{ display: 'flex', flexWrap: 'wrap-reverse' }} className="mb-5 justify-content-center">
 
-                    <Col>
-                        <Card>
-                            <Card.Header>Movie Title</Card.Header>
-                                
-                            <Card.Body>
-                                <ListGroup>
-                                    <ListGroup.Item><strong>Episode</strong> NaN</ListGroup.Item>
-                                    <ListGroup.Item><strong>Released</strong> NaN</ListGroup.Item>
-                                    <ListGroup.Item>NaN <strong>Characters</strong></ListGroup.Item>
-                                </ListGroup>
+                        {movies.map(movie => (
+                            <Col key={movie.episode_id}>
+                            <Card style={{ width: '24rem' }} className="mb-5">
+                                <Card.Header>{movie.title}</Card.Header>
 
-                                <br />
+                                <Card.Body>
+                                    <ListGroup>
+                                        <ListGroup.Item><strong>Episode</strong> {movie.episode_id}</ListGroup.Item>
+                                        <ListGroup.Item><strong>Released</strong> {movie.release_date}</ListGroup.Item>
+                                        <ListGroup.Item>{movie.characters.length} <strong>Characters</strong></ListGroup.Item>
+                                    </ListGroup>
 
-                                <div className="justify-content-md-center d-flex">
-                                    <Button as={Link} to="/movies/:id">Read more</Button>
-                                </div>
+                                    <br />
 
-                                    
-                            </Card.Body>
+                                    <div className="justify-content-md-center d-flex">
+                                        <Button as={Link} to={`/movies/:id`}>Read more</Button>
+                                    </div>
+                                </Card.Body>
                         
-                        </Card>
-                    </Col>
+                            </Card>
+                            </Col> 
+                        ))}
 
-                    
+
+                           
     
         </div>
     )
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,21 +1,53 @@
 import { Container, Row } from "react-bootstrap"
+import { useEffect, useState } from "react"
 import MovieCard from "../components/MovieCard"
+import SWApi from '../services/SWApi'
 
 const Movies = () => {
+    const [movies, setMovies] = useState([])
+    const [data, setData] = useState([])
+    const [showMovies, setShowMovies] = useState(true)
+    const [page, setPage] = useState(1)
+
+    const getMovies = async (page) => {
+        const data = await SWApi.getMovies(page)
+
+        setData(data)
+        setMovies(data.results)
+
+        console.log(data.results)
+
+        console.log(movies)
+    }
+
+    useEffect(() => {
+        getMovies(page)
+    }, [page])
+
+    if (!movies) {
+        return <p>Loading...</p>
+    }
+
     return (
         <div>
             <br />
 
             <h1>Movies</h1>
 
-            <Container>
+            <Container style={{}}>
                 <Row>
+                    {/* <MovieCard />
                     <MovieCard />
                     <MovieCard />
                     <MovieCard />
                     <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
+                    <MovieCard /> */}
+                    
+                    {showMovies ? (
+                        <>
+                            <MovieCard movies={movies} />
+                        </>
+                    ): null}
                 </Row>
             </Container>
              
